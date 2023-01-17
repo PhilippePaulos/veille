d1d569495d1ea9bac358226177e392dea8bb1518
Philippe P <philippe.paulos@gmail.com>
2023-01-17 15:23:04 +0100

    read file content

--- python_docker/python_docker/main.py
+++ python_docker/python_docker/main.py
@@ -1,18 +1,27 @@
 import argparse
+from typing import Tuple, Any
 
 
-def get_user():
+def get_args() -> tuple[str, str]:
     parser = argparse.ArgumentParser()
     parser.add_argument("--name", help="user's name")
+    parser.add_argument("--file", help="file's path")
     args = parser.parse_args()
-    return args.name
+    return args.name, args.file
 
 
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_hi(input_name):
+    print(f'Hi, {input_name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def print_file(path):
+    with open(path) as file:
+        print(file.read())
 
 
 if __name__ == '__main__':
+    name, file_path = get_args()
 
-    print_hi(get_user())
+    print_hi(name)
+    print_file(file_path)
 
